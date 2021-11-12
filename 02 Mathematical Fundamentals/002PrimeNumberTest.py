d892f8c52508a0a279615f40f88179460cf2c47f
Younis Rahman <younisrahman@Younis-MacBook-Pro-17.local>
2021-11-12 18:48:02 +0600

    number

---
+++ 02 Mathematical Fundamentals/002PrimeNumberTest.py
@@ -0,0 +1,11 @@
+def isPrime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+n = int(input("Please enter a number: "))
+print(isPrime(n))
