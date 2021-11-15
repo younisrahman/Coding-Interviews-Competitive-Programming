4671b2efe913431ae45d435f0eabc667ebbdda27
Younis Rahman <younisrahman@Younis-MacBook-Pro-17.local>
2021-11-15 11:43:06 +0600

    SieveofEratosthenes.py

--- 02 Mathematical Fundamentals/004PrimeFactorization.py
+++ 02 Mathematical Fundamentals/004PrimeFactorization.py
@@ -1,14 +1,45 @@
+# First approach: brute force
+# def prime_factor(n):
+#     list_factors = []
+#     i = 2
+#     while i <= n:
+#         if n % i == 0:
+#             list_factors.append(i)
+#             n = n / i
+#         else:
+#             i += 1
+#     return list_factors
+
+# First approach: brute force with exponent
+# def prime_factor(n):
+#     list_factors = []
+#     for i in range(2, n + 1):
+#         count = 0
+
+#         while n % i == 0:
+#             count += 1
+#             n = n / i
+#         if count > 0:
+#             list_factors.append((i, count))  # (i, count)
+#     return list_factors
+
+# Third approach: O(sqrt(n))
+import math  # sqrt
+
 
 def prime_factor(n):
     list_factors = []
-    i = 2
-    while i <= n:
-        if n % i == 0:
-            list_factors.append(i)
+    for i in range(2, int(math.sqrt(n))):
+        count = 0
+
+        while n % i == 0:
+            count += 1
             n = n / i
-        else:
-            i += 1
+        if count > 0:
+            list_factors.append((i, count))  # (i, count)
+    if n > 1:
+        list_factors.append((n, 1))  # (n, 1)
     return list_factors
 
 
-print(prime_factor(630))
+print(prime_factor(72))
---
+++ 02 Mathematical Fundamentals/SieveofEratosthenes.py
@@ -0,0 +1,21 @@
+from math import *
+
+
+def SieveofEratosthenes(n):
+    prime = [True] * (n+1)
+    p = prime[0] = prime[1] = False  # 0 and 1 are not prime
+    for i in range(2, int(sqrt(n))+1):
+        if prime[i]:
+            for j in range(i*i, n+1, i):
+                prime[j] = False
+    return prime
+
+
+t = int(input("Please enter a number how many input you give : "))
+for i in range(t):
+    n = int(input("Please enter a number for prime search : "))
+    sieve = SieveofEratosthenes(n)
+    for j in range(n+1):
+        if sieve[j]:
+            print(j, end=" ")
+    print()
